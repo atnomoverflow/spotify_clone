ccd79ad8864a315e116dfa8bd7622409d25d7de3
MedFarouk-ops <medfatouk55@gmail.com>
2021-10-05 22:12:36 +0200

    entity song commit 2nd

--- Entities/Song.cs
+++ Entities/Song.cs
@@ -4,5 +4,8 @@ namespace spotify_clone.Entities
     public record Song
     {
         public Guid Id { get; init; }
+        public string Name { get; set; }
+        public float Duration { get; set; }
+
     }
-}
+} 
