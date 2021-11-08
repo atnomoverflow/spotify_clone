f50567c724b52ca472334f749960d3d439d2fc65
MedFarouk-ops <medfatouk55@gmail.com>
2021-11-08 18:45:28 +0100

    project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Spotify_clone2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
